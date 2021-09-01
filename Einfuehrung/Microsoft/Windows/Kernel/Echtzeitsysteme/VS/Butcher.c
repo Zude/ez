bb359eeace0a5f05e0a6fe762ddecc09cd743ed1
Alexander Löffler <Alex.93.loeffler@me.com>
2021-09-01 19:40:01 +0200

    print fast fertig

--- Einfuehrung/Microsoft/Windows/Kernel/Echtzeitsysteme/VS/Butcher.c
+++ Einfuehrung/Microsoft/Windows/Kernel/Echtzeitsysteme/VS/Butcher.c
@@ -9,9 +9,12 @@
 OS_TMR* SausageTimer;
 boolean timerUsedFlag = 1;
 
-char* event1;
-char* event2;
-char* event3;
+char* event1 = "";
+char* event2 = "";
+char* event3 = "";
+char* event4 = "";
+char* event5 = "";
+char* event6 = "";
 
 void MyTmrCallbackFnct1(void* p_arg) {
 	INT8U err;
@@ -30,55 +33,64 @@ void MyTmrCallbackFnct1(void* p_arg) {
 }
 
 
-static void printState(char* event) {
+ void printState(char* event) {
+
+	event1 = event2;
+	event2 = event3;
+	event3 = event4;
+	event4 = event5;
+	event5 = event6;
+	event6 = event;
 
 	char buf[30];
 
 	PC_DispClrScr(5);
 
-		PC_DispStr(0, 0, "##############################################################", 5);
+	PC_DispStr(0, 0, "##############################################################", 6);
+	PC_DispStr(0, 1, "Events", 6);
+	PC_DispStr(0, 2, event1, 6);
+	PC_DispStr(0, 3, event2, 6);
+	PC_DispStr(0, 4, event3, 6);
+	PC_DispStr(0, 5, event4, 6);
+	PC_DispStr(0, 6, event5, 6);
+	PC_DispStr(0, 7, event6, 6);
+	PC_DispStr(0, 8, "##############################################################", 6);
+	
 	
 
-	PC_DispStr(0, 3, "##############################################################", 3);
-	PC_DispStr(15, 4, "Kuehlbox", 3);
+	PC_DispStr(0, 9, "##############################################################", 3);
+	PC_DispStr(15, 10, "Kuehlbox", 3);
 
 	snprintf(buf, 30, "Anzahl Wuerste: %d", sausagesCountBox); // puts string into buffer
-	PC_DispStr(10, 5, buf, 3);
+	PC_DispStr(10, 11, buf, 3);
 
 	for (size_t i = 0; i < sausagesCountBox; i++) {
-		PC_DispStr(15 + i, 6, "/", 3);
+		PC_DispStr(15 + i, 12, "/", 3);
 	}
-	PC_DispStr(0, 7, "##############################################################", 3);
-
-	PC_DispStr(0, 9, "##############################################################", 4);
-	PC_DispStr(15, 10, "Grill", 4);
+	PC_DispStr(0, 13, "##############################################################", 3);
+	PC_DispStr(0, 14, "##############################################################", 4);
+	PC_DispStr(0, 15, "Grill", 4);
 
 	for (size_t i = 0; i < sausagesCountGrill; i++) {
 		
 		SausageNode currentSaus = GetWurstAtIndex(sausagesCountGrill);
-			PC_DispStr(15, 11, "|", 4);
+			PC_DispStr(0, 16, "|", 4);
 			snprintf(buf, 30, "%d%", currentSaus->value.sideOne);
-			PC_DispStr(16, 11, buf, 4);
+			PC_DispStr(1, 16, buf, 4);
 
-			PC_DispStr(21, 11, "|", 4);
+			PC_DispStr(6, 16, "|", 4);
 			snprintf(buf, 30, "%d%", currentSaus->value.sideTwo);
-			PC_DispStr(22, 11, buf, 4);
+			PC_DispStr(7, 16, buf, 4);
 		
-			PC_DispStr(27, 11, "|", 4);
+			PC_DispStr(12, 16, "|", 4);
 			snprintf(buf, 30, "%d%", currentSaus->value.sideThree);
-			PC_DispStr(28, 11, buf, 4);
+			PC_DispStr(13, 16, buf, 4);
 
-			PC_DispStr(33, 11, "|", 4);
+			PC_DispStr(18, 16, "|", 4);
 			snprintf(buf, 30, "%d%", currentSaus->value.sideFour);
-			PC_DispStr(34, 11, buf, 4);
+			PC_DispStr(19, 16, buf, 4);
 		
 	}
-
-	PC_DispStr(0, 15, "##############################################################", 4);
-	PC_DispStr(15, 15, "Events", 4);
-	PC_DispStr(0, 15, "##############################################################", 4);
-
-
 
 }
 
@@ -94,7 +106,7 @@ static void printState(char* event) {
 
 	while (1) {
 
-		printState("");
+		printState("hez");
 
 		//PC_DispClrScr();
 		// Input des Users per MQueue checken
