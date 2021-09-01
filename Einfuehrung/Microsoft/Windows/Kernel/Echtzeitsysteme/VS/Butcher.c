f90342b1d30e2cfa64a2f0752cd059ea652aca1a
Alexander Löffler <Alex.93.loeffler@me.com>
2021-09-01 20:17:41 +0200

    dies das

--- Einfuehrung/Microsoft/Windows/Kernel/Echtzeitsysteme/VS/Butcher.c
+++ Einfuehrung/Microsoft/Windows/Kernel/Echtzeitsysteme/VS/Butcher.c
@@ -10,6 +10,7 @@
 #include  "InputListener.h"
 #include  "CoolingBox.h"
 #include  "Sausage.h"
+#include  "Helper.h"
 
 /*
 *********************************************************************************************************
@@ -20,17 +21,7 @@ OS_TMR* SausageTimer;
 volatile boolean timerUsedFlag = 1;
 
 
-/*
-*********************************************************************************************************
-*                                       LOCAL GLOBAL VARIABLES
-*********************************************************************************************************
-*/
-char* event1 = "";
-char* event2 = "";
-char* event3 = "";
-char* event4 = "";
-char* event5 = "";
-char* event6 = "";
+
 
 /*
 *********************************************************************************************************
@@ -54,66 +45,7 @@ void TimerCreateSausageCallback(void* p_arg) {
 }
 
 
- void printState(char* event) {
-
-	event1 = event2;
-	event2 = event3;
-	event3 = event4;
-	event4 = event5;
-	event5 = event6;
-	event6 = event;
-
-	char buf[30];
-
-	PC_DispClrScr(5);
-
-	PC_DispStr(0, 0, "##############################################################", 6);
-	PC_DispStr(0, 1, "Events", 6);
-	PC_DispStr(0, 2, event1, 6);
-	PC_DispStr(0, 3, event2, 6);
-	PC_DispStr(0, 4, event3, 6);
-	PC_DispStr(0, 5, event4, 6);
-	PC_DispStr(0, 6, event5, 6);
-	PC_DispStr(0, 7, event6, 6);
-	PC_DispStr(0, 8, "##############################################################", 6);
-	
-	
-
-	PC_DispStr(0, 9, "##############################################################", 3);
-	PC_DispStr(15, 10, "Kuehlbox", 3);
-
-	snprintf(buf, 30, "Anzahl Wuerste: %d", sausagesCountBox); // puts string into buffer
-	PC_DispStr(10, 11, buf, 3);
-
-	for (size_t i = 0; i < sausagesCountBox; i++) {
-		PC_DispStr(15 + i, 12, "/", 3);
-	}
-	PC_DispStr(0, 13, "##############################################################", 3);
-	PC_DispStr(0, 14, "##############################################################", 4);
-	PC_DispStr(0, 15, "Grill", 4);
-
-	for (size_t i = 0; i < sausagesCountGrill; i++) {
-		
-		SausageNode currentSaus = GetWurstAtIndex(sausagesCountGrill);
-			PC_DispStr(0, 16, "|", 4);
-			snprintf(buf, 30, "%d%", currentSaus->value.sideOne);
-			PC_DispStr(1, 16, buf, 4);
-
-			PC_DispStr(6, 16, "|", 4);
-			snprintf(buf, 30, "%d%", currentSaus->value.sideTwo);
-			PC_DispStr(7, 16, buf, 4);
-		
-			PC_DispStr(12, 16, "|", 4);
-			snprintf(buf, 30, "%d%", currentSaus->value.sideThree);
-			PC_DispStr(13, 16, buf, 4);
-
-			PC_DispStr(18, 16, "|", 4);
-			snprintf(buf, 30, "%d%", currentSaus->value.sideFour);
-			PC_DispStr(19, 16, buf, 4);
-		
-	}
-
-}
+ 
 
 
  void Butcher(void* p_arg) {
@@ -122,10 +54,12 @@ void TimerCreateSausageCallback(void* p_arg) {
 
 	while (1) {
 
-		printState("hez");
+		printCurrentState("ds");
 
 		//PC_DispClrScr();
 		// Input des Users per MQueue checken
+
+
 		char userInput = (char*)OSQAccept(msgQueueButcher, &err);
 
 		// Wenn der Input korrekt ist, Wurst erzeugen. Sonst schauen ob Box Leer und ggf. Timer starten (60 Sekunden)
